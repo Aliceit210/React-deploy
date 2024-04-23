2e55cb0f9597023062f5fce91ab8e897cf4dc859
ImmaculeeM <imushime94@gmail.com>
2024-04-23 11:51:19 +0200

    file saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
@@ -5,31 +6,40 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+
+  const handlePageClick = (page) => {
+    setSelectedPage(page);
+  };
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <nav>
-          <div>
-            <ul>
+    <div className="pink-background flex">
+      <section className='w-full md:w-1/4 shadow-md mt-1 md:mt h-full'>
+        
+        <div className='text-blue-500 text-lg font-bold mt- bg-green-200 p-4 mb-3'>Uwimana Alice</div>
+        
+        <Router>
+          <nav>
+            <ul className='flex flex-col mt-8'>
               <li>
-                <Link to="/">Home</Link>
+                <Link to="/" onClick={() => handlePageClick('home')} className="page-link">Home</Link>
               </li>
               <li>
-                <Link to="/about">About me</Link>
+                <Link to="/about" onClick={() => handlePageClick('about')} className="page-link" >About me</Link>
               </li>
               <li>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contact" onClick={() => handlePageClick('contact')} className="page-link">Contact</Link>
               </li>
             </ul>
-          </div>
-        </nav>
-      </Router>
-    </>
+          </nav>
+        </Router>
+      </section>
+      <section className='flex-1 bg-yellow-200 shadow-md p-4'>
+        {selectedPage === 'home' && <Home />}
+        {selectedPage === 'about' && <About />}
+        {selectedPage === 'contact' && <Contact />}
+      </section>
+    </div>
   );
 }
 
